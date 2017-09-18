eee69ab5ddb74d95d5e38a6cf0f280b99e855a1c
echai13 <cher13@brandeis.edu>
2017-09-18 10:38:41 -0400

    cat, head, tail

--- bash.js
+++ bash.js
@@ -4,10 +4,12 @@ process.stdout.write('prompt: ');
 process.stdin.on('data', function (data) {
     var cmd = data.toString().trim().split(' ');
     var rest = cmd.slice(1).join(' ');
-    
+
     if (cmd[0] === 'pwd')  {command.pwd();}
     else if (cmd[0] === 'ls')   {command.ls();}
     else if (cmd[0] === 'date')    {command.date();}
     else if (cmd[0] === 'echo')    {command.echo(rest);}
-
+    else if(cmd[0] === 'cat') {command.cat(rest);}
+    else if (cmd[0] === 'head') {command.head(rest);}
+    else if (cmd[0] === 'tail') {command.tail(rest);}
 });
--- command.js
+++ command.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
-function pwd ()   {
+function pwd (str)   {
     process.stdout.write(process.argv[1]);
     process.stdout.write('\nprompt: ');
 }
-function date ()    {
+function date (str)    {
     process.stdout.write(Date());
-    process.stdout.write('\nprompt: ');    
+    process.stdout.write('\nprompt: ');
 }
-function ls ()    {
+function ls (str)    {
     fs.readdir('.', function (err, files)   {
         if (err) {process.stdout.write('Error');}
         files.forEach(function(file)    {
@@ -21,4 +21,46 @@ function echo(str)  {
     process.stdout.write('\nprompt: ');
 }
 
-module.exports = {pwd, ls, date, echo};
+function cat(str) {
+  fs.readFile(str, function read(err, data) {
+    if(err) {
+      throw "error";
+    } else {
+      process.stdout.write(data);
+    }
+    process.stdout.write('\nprompt: ');
+  });
+
+}
+
+function head(str) {
+  fs.readFile(str, function read(err, data) {
+    if(err) {
+      throw "error";
+    } else {
+      var lines = data.toString().split("\n");
+      for(var i = 0; i < 5; i++) {
+        process.stdout.write(lines[i] + "\n");
+      }
+    }
+    process.stdout.write('\nprompt: ');
+  });
+
+}
+
+function tail(str) {
+  fs.readFile(str, function read(err, data) {
+    if(err) {
+      throw "error";
+    } else {
+      var lines = data.toString().split("\n");
+      for(var i = lines.length - 5; i < lines.length; i++) {
+        process.stdout.write(lines[i] + "\n");
+      }
+    }
+    process.stdout.write('\nprompt: ');
+  });
+
+}
+
+module.exports = {pwd, ls, date, echo, cat, head, tail};
